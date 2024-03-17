274557c8aedf127c3f33358345b7af11d943116a
khalidzahra <khalidzahra923@gmail.com>
2024-03-17 04:37:36 +0200

    Green make indexOfString pass tests

--- src/main/java/me/khalid/MyString.java
+++ src/main/java/me/khalid/MyString.java
@@ -3,6 +3,7 @@ package me.khalid;
 public class MyString {
 
     public int indexOfString(String s1, String s2, int pos) {
+        if (s1 == null || s2 == null) return -1;
         return -10;
     }
 
